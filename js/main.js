530f2a44db27739179f1ce0287e330f038de8424
Vishesh <visheshsingh16@gmail.com>
2017-04-23 21:11:28 -0400

    Sections: about, resume added along with animations

---
+++ js/main.js
@@ -0,0 +1,109 @@
+window.sr = ScrollReveal();
+sr.reveal('.navbar', {
+  duration: 2000,
+  origin: 'bottom'
+});
+
+sr.reveal('.showcase-left', {
+  duration: 2000,
+  origin: 'top',
+  distance: '300px'
+});
+
+sr.reveal('.showcase-right', {
+  duration: 2000,
+  origin: 'right',
+  distance: '300px'
+});
+
+sr.reveal('.showcase-btn', {
+  duration: 2000,
+  origin: 'bottom',
+  delay: 2000
+});
+
+sr.reveal('#testimonial div', {
+    duration: 2000,
+    origin: 'bottom',
+});
+
+sr.reveal('#about', {
+    duration: 2000,
+    origin: 'bottom',
+});
+
+sr.reveal('#about p', {
+    duration: 2000,
+    origin: 'top',
+    delay: 1000
+});
+
+sr.reveal('#code', {
+    duration: 2000,
+    origin: 'bottom',
+    distance: '300px'
+});
+
+sr.reveal('.info-left', {
+    duration: 2000,
+    origin: 'left',
+    distance: '300px',
+    viewFactor: 0.2
+});
+
+sr.reveal('.info-right', {
+    duration: 2000,
+    origin: 'right',
+    distance: '300px',
+    viewFactor: 0.2
+});
+
+sr.reveal('#contact form', {
+    duration: 2000,
+    origin: 'left',
+    distance: '300px',
+    viewFactor: 0.2
+});
+
+sr.reveal('.img-left', {
+    duration: 2000,
+    origin: 'left',
+    distance: '300px',
+    viewFactor: 0.5,
+    delay: 800,
+});
+
+sr.reveal('.img-right', {
+    duration: 2000,
+    origin: 'right',
+    distance: '300px',
+    viewFactor: 0.5,
+    delay: 800,
+});
+
+sr.reveal('#description', {
+    duration: 2000,
+    origin: 'bottom',
+    distance: '300px',
+});
+
+sr.reveal('#description p', {
+    duration: 2000,
+    origin: 'top',
+    distance: '300px',
+    delay: 800,
+});
+
+sr.reveal('.skills-left', {
+    duration: 2000,
+    origin: 'left',
+    distance: '300px',
+    viewFactor: 0.2,
+});
+
+sr.reveal('.skills-right', {
+    duration: 2000,
+    origin: 'right',
+    distance: '300px',
+    viewFactor: 0.2
+});
